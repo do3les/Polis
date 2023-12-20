03dfea1edc280df88cb42332b9184609081bb2c1
leo <leo.schreiber@posteo.de>
2023-12-20 17:58:38 +0100

    basic raylib setup

--- main.cpp
+++ main.cpp
@@ -1,6 +1,44 @@
-#include <iostream>
+// Polis City Simulator
 
-int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+/*--- includes ---*/
+#include "raylib.h"
+#include "raymath.h"
+
+/*--- defines ---*/
+
+
+/*--- globals ---*/
+const int windowWidth = 1000;
+const int windowHeight = 1000;
+const Vector2 windowCenter = {windowWidth / 2.0f, windowHeight / 2.0f};
+
+
+/*--- main ---*/
+int main(void){
+    /*--- pre-init ---*/
+
+
+    /*--- init ---*/
+    InitWindow(windowWidth, windowHeight, "Polis - City Simulator");
+
+
+    /*--- game loop ---*/
+    while(!WindowShouldClose()){
+        /*--- pre-draw ---*/
+
+
+        /*--- draw ---*/
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+        EndDrawing();
+
+        /*--- post-draw ---*/
+
+
+    }
+
+    /*--- terminate ---*/
+    CloseWindow();
 }
