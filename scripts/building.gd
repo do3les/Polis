3e0da3239467393796c10c029a06144aec07efc4
leo <leo.schreiber@posteo.de>
2024-02-27 19:54:46 +0100

    moved building patterns to separate files

--- scripts/building.gd
+++ scripts/building.gd
@@ -1,10 +1,16 @@
 extends Node2D
 class_name Building
 
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	%TileMap.set_pattern(1, self.position, %TileMap.tile_set.get_pattern(0))
-	%TileMap.set_pattern(2, self.position + Vector2(0, -2), %TileMap.tile_set.get_pattern(1))
+	var buildingPattern = load("res://assets/buildings/house0/building.tres")
+	var roofPattern = load("res://assets/buildings/house0/roof.tres")
+	
+	%TileMap.set_pattern(1, self.position, buildingPattern)
+	%TileMap.set_pattern(2, self.position + Vector2(0, -2), roofPattern)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
