855ecd9913b3ffa76cc68c825440fb157e2f39ab
Physalis <143352299+Physalis2@users.noreply.github.com>
2024-02-27 23:54:34 +0100

    jao funktioniert so halb

--- scripts/controller.gd
+++ scripts/controller.gd
@@ -1,11 +1,36 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func _physics_process(delta):
+	time_passing(gameSpeed)
+	print(str(hour) + ":" + str(minute) +  " " + str(day) + "." + str(month) + "." + str(year))
+	print("one year")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+
+#Time
+var gameSpeed = 1
+var minute = 0
+var hour = 0
+var day = 0
+var month = 0
+var year = 0
+
+func time_passing(speed):
+	while speed < 0:
+		await get_tree().create_timer(20/speed).timeout
+		print("hi")
+		minute += 10
+		if minute >= 60:
+			minute = 0
+			hour += 1
+			if hour >= 24:
+				hour = 0
+				day += 1
+				if day >= 30:
+					day = 0
+					month += 1
+					if month >= 12:
+						month = 0 
+						year += 1
