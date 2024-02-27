f576b8d06ebea07f6b1ac0c0fc5d29079cafdeba
leo <leo.schreiber@posteo.de>
2024-02-27 17:37:38 +0100

    minor fix

--- scripts/building.gd
+++ scripts/building.gd
@@ -3,8 +3,8 @@ class_name Building
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	%TileMap.set_pattern(1, self.global_position, %TileMap.tile_set.get_pattern(0))
-	%TileMap.set_pattern(2, self.global_position + Vector2(0, -2), %TileMap.tile_set.get_pattern(1))
+	%TileMap.set_pattern(1, self.position, %TileMap.tile_set.get_pattern(0))
+	%TileMap.set_pattern(2, self.position + Vector2(0, -2), %TileMap.tile_set.get_pattern(1))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
